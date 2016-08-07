4fe93dee08bae1b7756b5c9f298b25fd861c7d45
Robert Shonfelt <r.shonfelt@gmail.com>
2016-08-07 14:35:10 -0400

    Will allow a simple LAN game to be played locally. Simply build and run a standalone application as server host, then run from Unity editor as client. Can move ball around with WASD. Spheres should appear blue on local game and green on the other.

---
+++ PongNetProject/Assets/Controller.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Controller : NetworkBehaviour {
+
+    float Xcoord; // X coordinate
+    float Zcoord; // Z coordinate
+    public GameObject mainCamera;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        Xcoord += Input.GetAxis("Horizontal") * Time.deltaTime;
+        Zcoord += Input.GetAxis("Vertical") * Time.deltaTime;
+
+        transform.Translate(Xcoord, 0, Zcoord);
+        mainCamera.transform.Translate(Xcoord, 0, Zcoord);
+        mainCamera.transform.LookAt(gameObject.transform);
+    }
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+}
